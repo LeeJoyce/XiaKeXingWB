68ee27ce66d71dc01d6a694decf1e4c420ebc396
wangzhongyi <wangzongyi@clickwifi.net>
2017-05-19 10:49:16 +0800

    Initial Commit

---
+++ XiaKeXing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiaKeXing
+//
+//  Created by JOY on 17/5/19.
+//  Copyright © 2017年 JOY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XiaKeXing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XiaKeXing
+//
+//  Created by JOY on 17/5/19.
+//  Copyright © 2017年 JOY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
